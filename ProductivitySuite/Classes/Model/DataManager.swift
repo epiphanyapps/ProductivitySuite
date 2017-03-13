a8fa40389fd2a7be9441844c24973101f8a8d86d
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2017-03-13 13:51:31 -0400

    typo

--- ProductivitySuite/Classes/Model/DataManager.swift
+++ ProductivitySuite/Classes/Model/DataManager.swift
@@ -10,7 +10,6 @@ import Foundation
 
 ///http://stackoverflow.com/questions/29670585/how-to-convert-unix-epoc-time-to-date-and-time-in-ios-swift
 func unixEpocToDate(timeStamp: Double) -> NSDate {
-    let epocTime = TimeInterval(1429162809359) / 1000
-    print("Converted Time \(timeStamp)")
+    let epocTime = TimeInterval(timeStamp) / 1000
     return NSDate(timeIntervalSince1970:  epocTime)
 }
