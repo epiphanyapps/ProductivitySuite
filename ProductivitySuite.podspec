341cf2bb86f3e37d166c464f9f78fa709fc65630
Walter Vargas-Pena <walter.vargaspena@gmail.com>
2017-03-16 21:19:09 -0400

    pod cleanup docs

--- ProductivitySuite.podspec
+++ ProductivitySuite.podspec
@@ -40,4 +40,5 @@ TODO: Add long description of the pod here.
   # s.frameworks = 'UIKit', 'MapKit'
   s.dependency 'Alamofire', '~> 4.4.0'
   s.dependency 'Genome'
+  
 end
--- ProductivitySuite/Classes/Model/DataManager.swift
+++ ProductivitySuite/Classes/Model/DataManager.swift
@@ -6,8 +6,7 @@
 //
 //
 
-import Foundation
-
+import UIKit
 ///http://stackoverflow.com/questions/29670585/how-to-convert-unix-epoc-time-to-date-and-time-in-ios-swift
 func unixEpocToDate(timeStamp: Double) -> NSDate {
     let epocTime = TimeInterval(timeStamp) / 1000
